65f45015dfa62aa2f09922fd0d251bef8a87be2f
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2017-04-21 12:50:00 +0700

    Updates

--- lib/wp_wrapper/modules/themes.rb
+++ lib/wp_wrapper/modules/themes.rb
@@ -2,25 +2,25 @@ module WpWrapper
   module Modules
     module Themes
       
-      def activate_random_theme
-        theme_links               =   get_theme_links
+      def activate_random_theme(ignore_themes: [])
+        theme_links               =   get_theme_links(ignore_themes: ignore_themes)
         random_link               =   (theme_links && theme_links.any?) ? theme_links.to_a.sample : nil
 
-        perform_activation(random_link["href"]) if (random_link)
+        perform_activation(random_link["href"]) if random_link
       end
     
       def activate_theme(theme_identifier = 'twentytwelve')
         success                   =   false
         theme_links               =   get_theme_links
         
-        if (theme_links && theme_links.any?)
+        if theme_links && theme_links.any?
           activation_link         =   nil
           regex                   =   Regexp.new("stylesheet=#{theme_identifier}", Regexp::IGNORECASE)
       
           theme_links.each do |link|
             href                  =   link["href"]
         
-            if (regex.match(href))
+            if regex.match(href)
               activation_link     =   href
               break
             end
@@ -39,29 +39,43 @@ module WpWrapper
       end
       
       def perform_activation(url)
-        success                 =   false
+        success             =   false
         
-        if (url && url.present?)
+        if url && url.present?
           puts "[WpWrapper::Modules::Themes] - #{Time.now}: Will activate theme with url #{url}."
           self.mechanize_client.open_url(url)
-          success               =   true
+          success           =   true
         end
         
         return success
       end
       
-      def get_theme_links
-        theme_links           =   []
+      def get_theme_links(ignore_themes: [])
+        theme_links         =   []
         
-        if (login)
-          themes_page         =   self.mechanize_client.open_url(get_url(:themes))
+        if login
+          themes_page       =   self.mechanize_client.open_url(get_url(:themes))
+          links             =   themes_page ? themes_page.parser.css("div.themes div.theme div.theme-actions a.activate") : []
           
-          if (themes_page)
-            theme_links       =   themes_page.parser.css("div.themes div.theme div.theme-actions a.activate")
+          if ignore_themes && ignore_themes.any?
+            links.each do |link|
+              href          =   link["href"]
+            
+              ignore_themes.each do |ignore_theme|
+                regex       =   Regexp.new("stylesheet=#{ignore_theme}", Regexp::IGNORECASE)
+            
+                if !regex.match(href)
+                  theme_links << href
+                  break
+                end
+              end
+            end if links && links.any?
+          else
+            theme_links     =   links
           end
         end
         
-        puts "[WpWrapper::Modules::Themes] - #{Time.now}: Found a total of #{theme_links.size} theme activation links. Maybe there's only one theme available?"
+        puts "[WpWrapper::Modules::Themes] - #{Time.now}: Found a total of #{theme_links.size} theme activation links."
         
         return theme_links
       end
